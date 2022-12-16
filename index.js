01c229cf8bc58c70fc7c4be67e5f8179b2778c5e
roy <kimathiwaweru@gmail.com>
2022-12-16 16:38:35 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
+    let button = document.getElementById("button")
+    button.addEventListener("click", ()=> {alert("i have been clicked")})
 }
+addingEventListener();
